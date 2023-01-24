766a033cb9520743ac00fe97bd636fc875c02909
Soquat <msoquat@questax.com>
2023-01-24 08:33:01 +0100

    vuex + axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,33 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
   },
   getters: {
   },
   mutations: {
+    increment(state, payload) {
+      state.counter += payload;
+    },
+    decrement(state, payload) {
+      state.counter -= payload;
+    },
   },
   actions: {
+    increment({ commit }) {
+      console.log('increment (action)')
+      axios('https://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1').then(res => {
+        commit('increment', res.data[0])
+      })
+    },
+    decrement({ commit }) {
+      axios('https://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1').then(res => {
+        commit('decrement', res.data[0])
+      })
+
+    },
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter">{{ $store.state.counter }}</div>
+    <div class="buttons">
+      <button @click="$store.dispatch('decrement')">-</button>
+      <button @click="$store.dispatch('increment')">+</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
