6222785f1283ad55db00ba4f11d32d3de5a93d03
Soquat <msoquat@questax.com>
2023-01-24 08:48:23 +0100

    added color-state

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,24 @@ import axios from 'axios'
 export default createStore({
   state: {
     counter: 0,
+    colorCode: 'blue',
   },
   getters: {
+
+    counterSquared(state) {
+      return state.counter * state.counter
+    }
   },
   mutations: {
-    increment(state, payload) {
-      state.counter += payload;
+    increment(state, randomNumber) {
+      state.counter += randomNumber;
     },
-    decrement(state, payload) {
-      state.counter -= payload;
+    decrement(state, randomNumber) {
+      state.counter -= randomNumber;
     },
+    setColorCode(state, colorCode) {
+      state.colorCode = colorCode;
+    }
   },
   actions: {
     increment({ commit }) {
@@ -26,8 +34,10 @@ export default createStore({
       axios('https://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1').then(res => {
         commit('decrement', res.data[0])
       })
-
     },
+    setColorCode({ commit }, colorCode) {
+      commit('setColorCode', colorCode)
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="counter">{{ $store.state.counter }}</div>
+    <div :style="{ color: $store.state.colorCode }" class="counter">
+      {{ $store.state.counter }}
+    </div>
+    <div class="counter-squard">
+      {{ $store.state.counter }}
+      <sup>2</sup> = {{ $store.getters.counterSquared }}
+    </div>
     <div class="buttons">
       <button @click="$store.dispatch('decrement')">-</button>
       <button @click="$store.dispatch('increment')">+</button>
+    </div>
+    <div>
+      <input v-model="colorCode" placeholder="Enter color!" type="text" />
     </div>
   </div>
 </template>
@@ -14,6 +23,16 @@
 
 export default {
   name: "HomeView",
+  computed: {
+    colorCode: {
+      get() {
+        return this.$store.state.colorCode;
+      },
+      set(value) {
+        this.$store.dispatch("setColorCode", value);
+      },
+    },
+  },
 };
 </script>
 
